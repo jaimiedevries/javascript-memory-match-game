abfa4bf4a3ea3fb4445918c651bdade381e28386
Jaimie de Vries <49305844+jaimiedevries@users.noreply.github.com>
2023-01-29 19:04:12 +0100

    card css edits and added a icon and bg image

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 
 const pokeAPIBaseUrl = 'https://pokeapi.co/api/v2/pokemon/';
 const cardGrid = document.getElementById('card-grid');
+const gameScore = document.getElementById('game-score');
+const turnScore = document.getElementById('turn-score');
+
 
 let isPaused = false;
 let firstPick;
 let matches;
+let gamesWon = 0;
+let turns = 0;
 
 const loadPokemon = async () => {
     const randomIds = new Set();
@@ -26,6 +31,7 @@ const displayPokemon = (pokemon) => {
                 <div class="grid-item" onclick="clickCard(event)" data-pokename="${pokemon.name}">
                     <div class="front">
                         <img src="/src/img/pokeball.svg"/>
+                        <img class="front__inner-border" src="/src/img/border__inner.png"/>
                     </div>
                     <div class="back rotated">
                         <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}"/>
@@ -57,6 +63,7 @@ const clickCard = (e) => {
             const [firstFront, firstBack] = getFrontAndBackFromCard(firstPick);
             setTimeout(() => {
                 rotateElements([front, back, firstFront, firstBack]);
+                updateTurnScore();
                 firstPick = null;
                 isPaused = false;
             }, 500);
@@ -64,12 +71,37 @@ const clickCard = (e) => {
         } else {
             matches++;
             if (matches === 8) {
-                console.log('winner');
+                setTimeout(async () => {
+                    updateGameScore();
+                    alert('Winner!');
+                    const pokemon = await loadPokemon();
+                    displayPokemon([...pokemon, ...pokemon]);
+                    turns = 0;
+                    turnScore.innerHTML = turns;
+                    isPaused = false;
+                }, 300);
+
             }
             firstPick = null;
             isPaused = false;
         }
     }
+}
+
+const updateGameScore = () => {
+    gamesWon++;
+    gameScore.innerHTML = gamesWon;
+}
+
+const updateTurnScore = () => {
+    if (turns <= 10) {
+        turns++;
+        turnScore.innerHTML = turns;
+    } else {
+        alert('loser!');
+        resetGame();
+    }
+
 }
 
 const rotateElements = (elements) => {
@@ -89,6 +121,10 @@ const resetGame = () => {
     isPaused = true;
     firstPick = null;
     matches = 0;
+    turns = 0;
+    gamesWon = 0;
+    gameScore.innerHTML = gamesWon;
+    turnScore.innerHTML = turns;
 
     setTimeout(async () => {
         const pokemon = await loadPokemon();
@@ -99,4 +135,4 @@ const resetGame = () => {
 
 resetGame();
 
-// Note: Event propagation -> Use event.currentTager.
+// Note: Event propagation -> Use event.currentTarget
